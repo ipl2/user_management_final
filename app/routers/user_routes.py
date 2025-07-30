3cf9b7f54d125494afbb2fa0115a8bd1b1cc9b11
ipl2 <ipl2@njit.edu>
2025-07-30 09:31:04 -0400

    added new route for registration process of users

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -277,6 +277,14 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Async
         return {"access_token": access_token, "token_type": "bearer"}
     raise HTTPException(status_code=401, detail="Incorrect email or password.")
 
+# adding new route for registering users
+@router.post("/register")
+async def register_user_route(
+    user_data: UserCreate,
+    db: AsyncSession = Depends(get_db),
+    email_service: EmailService = Depends(get_email_service),
+):
+    return await UserService.register_user(db, user_data.model_dump(), email_service)
 
 @router.get("/verify-email/{user_id}/{token}", status_code=status.HTTP_200_OK, name="verify_email", tags=["Login and Registration"])
 async def verify_email(user_id: UUID, token: str, db: AsyncSession = Depends(get_db), email_service: EmailService = Depends(get_email_service)):
